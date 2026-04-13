f2195ef2abfb9365dfd5527e7e787e3e7c79b509
Shahzaib222-Solangi <shahzaibalisolangi222@gmail.com>
2026-04-13 15:00:55 +0500

    Added assembly code to print numbers 1 to 9

---
+++ Loops/PrintingNaturalNumbers_from_1to9.asm
@@ -0,0 +1,22 @@
+.model small
+.stack 100h
+.code
+main proc
+    mov cx,9
+    mov bl,1
+    add bl,48  
+    
+      start:
+         
+          mov ah,02h
+          mov dl,bl
+          int 21h
+          
+          inc bl
+          
+      loop start
+   
+   mov ah,4ch
+   int 21h
+   main endp
+   end main
